14b96ab93847ad55a26506fe47a66c78e4446aa2
Bernardo David Nolasco Vargas <95383474+RogueBaker01@users.noreply.github.com>
2025-03-09 21:27:55 +0000

    modified codes

--- Save_Cloud_Points_PCD.py
+++ Save_Cloud_Points_PCD.py
@@ -1,11 +1,12 @@
 import numpy as np
 import open3d as o3d
 
-x = np.asarray("/Datos/array_x1.txt")
-y = np.asarray("Datos/array_y1.txt")
-z = np.asarray("Datos/array_z1.txt")
+x = np.loadtxt("Datos/array_x1.txt")
+y = np.loadtxt("Datos/array_y1.txt")
+z= np.loadtxt("Datos/array_z1.txt")
 
-xyz = x*y*z
+#Combina los arrays en una matriz
+xyz = np.column_stack((x,y,z))
 
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(xyz)
--- save_CLoud_Points_PLY.py
+++ save_CLoud_Points_PLY.py
@@ -1,10 +1,13 @@
 import numpy as np
 import open3d as o3d
 
-x = np.asarray("/Datos/array_x1.txt")
-y = np.asarray("Datos/array_y1.txt")
-z = np.asarray("Datos/array_z1.txt")
-xyz = x*y*z
+x = np.loadtxt("Datos/array_x1.txt")
+y = np.loadtxt("Datos/array_y1.txt")
+z= np.loadtxt("Datos/array_z1.txt")
+
+#Combina los arrays en una matriz
+xyz = np.column_stack((x,y,z))
+
 # Crea un objeto PointCloud de Open3D
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(xyz)
